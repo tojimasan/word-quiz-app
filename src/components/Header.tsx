31aee196041ae478157fa20f8b0fcd30c4c6401a
kazuto ushijima <ktojima0123@gmail.com>
2024-03-31 15:33:21 +0900

    menu with dropdown

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,11 +1,28 @@
 import { Button } from "@/components/ui/button";
-import { Menu } from "lucide-react";
+import { Menu, Github } from "lucide-react";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
 
 export const Header = () => (
   <div className="flex justify-between w-full p-4">
     <span className="font-bold text-slate-700">Word Quiz App</span>
-    <Button variant="ghost" size="icon">
-      <Menu className="h-4 w-4" />
-    </Button>
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <Button variant="ghost" size="icon" className="relative -top-2">
+          <Menu className="h-4 w-4" />
+        </Button>
+      </DropdownMenuTrigger>
+
+      <DropdownMenuContent className="w-56">
+        <DropdownMenuItem onSelect={() => window.open('https://github.com/tojimasan/word-quiz-app', '_blank')}>
+          <Github className="mr-2 h-4 w-4" />
+          <span>GitHub</span>
+        </DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
   </div>
 );
